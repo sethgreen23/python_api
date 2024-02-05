2e40140bd041053416d494940c94b92b5f6a105e
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-05 17:38:20 +0100

    construct first post request using fastapi

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello to my api"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "this is my post"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body):
+    print(payload)
+    return {"message": "post create successfull",
+            "data": f"title: {payload['title']}, content: {payload['content']}"}

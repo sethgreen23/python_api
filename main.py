7a6e473d00e1008f9efd76af6e1df2df1bedb8ed
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-05 18:45:37 +0100

    using pydantic to validate the frontend

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    rating: Optional[int] = None # optional and default to none
+    published: bool = True # optional
 
 @app.get("/")
 async def root():
@@ -11,7 +19,10 @@ async def get_posts():
     return {"data": "this is my post"}
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body):
-    print(payload)
-    return {"message": "post create successfull",
-            "data": f"title: {payload['title']}, content: {payload['content']}"}
+def create_posts(post: Post):
+    print(post)
+    # change pydantic model to dictionary
+    print(post.dict())
+    return {
+            "data": f"title: {post.title}, content: {post.content}",
+            "json_data": post.dict()}

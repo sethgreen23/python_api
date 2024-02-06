df0508b48217f77306eec40ccd51597c63e86b00
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-07 00:29:04 +0100

    add new post

--- app/main.py
+++ app/main.py
@@ -60,12 +60,11 @@ async def get_posts():
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {
-            "data": post_dict
-            }
+    cur.execute(""" INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING *""",
+                (post.title, post.content, post.published))
+    new_post = cur.fetchone()
+    conn.commit()
+    return {"data": new_post}
 
 @app.get("/posts/{id}")
 def get_post(id: int):
